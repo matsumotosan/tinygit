bd67d37fdb0426c683600b0e055a0a0dd8ec8b20
Shion <smatsumoto97@gmail.com>
2023-10-16 22:36:35 +0900

    import plumbing and porcelain subcommands

--- cmd/root.go
+++ cmd/root.go
@@ -2,24 +2,19 @@ package cmd
 
 import (
 	"os"
-	"tinygit/cmd/diff"
 	tinyinit "tinygit/cmd/init"
+	"tinygit/cmd/diff"
+	"tinygit/cmd/plumbing"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tinygit",
 	Short: "A tiny reimplementation of git in Go.",
 	Long: `A tiny reimplementation of git in Go.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -27,16 +22,17 @@ func Execute() {
 	}
 }
 
-func init() {
+func addPlumbing() {
+	rootCmd.AddCommand(plumbing.HashObjectCmd)
+	rootCmd.AddCommand(plumbing.CatFileCmd)
+}
+
+func addPorcelain() {
 	rootCmd.AddCommand(tinyinit.InitCmd)
 	rootCmd.AddCommand(diff.DiffCmd)
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+}
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tinygit.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func init() {
+	addPlumbing()
+	addPorcelain()
 }
